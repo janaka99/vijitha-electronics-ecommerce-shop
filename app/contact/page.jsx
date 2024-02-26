f16cf50e43982cf5128f09283708fcbeda034d17
Janaka Chamith <48000312+janaka99@users.noreply.github.com>
2024-02-26 16:11:29 +0530

    update

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -8,41 +8,41 @@ const page = () => {
   const contact = [
     {
       title: "Address",
-      text: "Yatinuwara Street <br> Kandy",
+      text: "Yatinuwara Street, Kandy",
       icon: FaLocationDot,
-      color: "#000000",
+      color: "text-gray-600",
     },
     {
       title: "Email",
       text: "vijithaelectronicskandy@gmail.com",
       icon: MdEmail,
-      color: "#000000",
+      color: "text-red-300",
     },
     {
       title: "Phone Number",
-      text: "+94 767661522 <br> +94 757669625",
+      text: "+94 767661522",
       icon: FaPhone,
-      color: "#000000",
+      color: "text-blue-500",
     },
     {
       title: "Whatsapp",
       text: "+94 767008571",
       icon: FaWhatsappSquare,
-      color: "#000000",
+      color: "text-green-500",
     },
   ];
 
   return (
     <div className="flex flex-col gap-5 p-12 min-h-[calc(100vh-240px)] max-w-[1440px] mx-auto w-[95%]">
       <h1 className="text-2xl font-semibold text-blue-600 mb-4">Contact Us</h1>
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {contact.map((c, i) => (
           <div
             key={i}
-            className="p-4 bg-white shadow-lg rounded-lg hover:shadow-xl hover:border-blue-600 border transition-transform transform hover:-translate-y-1 flex items-center"
+            className={` p-4 bg-white shadow-lg rounded-lg hover:shadow-xl hover:border-blue-600 border transition-transform transform hover:-translate-y-1 flex items-center`}
           >
             <div className="mr-4">
-              <c.icon size={50} color={c.color} />
+              <c.icon size={50} className={c.color} />
             </div>
             <div>
               <span className="block text-xl font-semibold text-blue-600 hover:underline">
